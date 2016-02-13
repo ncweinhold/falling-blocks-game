c6b6416a327b3217528f9eae3be0798e75a4e3c6
Nick Weinhold <n.c.weinhold@googlemail.com>
2016-02-13 19:28:52 +0000

    Simple UI rendering

--- falling-blocks-game.lisp
+++ falling-blocks-game.lisp
@@ -2,16 +2,36 @@
 
 (in-package #:falling-blocks-game)
 
-(defparameter *screen-width* 600)
-(defparameter *screen-height* 750)
+(defconstant +screen-width+ 650)
+(defconstant +screen-height+ 750)
 (defparameter *window* nil)
+(defparameter *score* 0)
+(defparameter *background-color* (sdl:color :r 0 :g 0 :b 150 :a 255))
+(defparameter *ui-font* (sdl:initialise-font sdl:*font-10x20*))
+
+(defun update-game ()
+  (write-line "Updating"))
+
+(defun render-game ()
+  (render-gui))
+
+(defun render-gui ()
+  (sdl:clear-display *background-color*)
+  (sdl:draw-box-* 10 10 420 730 :color sdl:*black*)
+  (sdl:draw-string-solid-* (format nil "Score: ~d" *score*)
+			   450 10
+			   :font *ui-font*)
+  (render-next-piece))
+
+(defun render-next-piece ()
+  (sdl:draw-string-solid-* "Next"
+			   450 60
+			   :font *ui-font*)
+  (sdl:draw-box-* 450 110 150 150 :color sdl:*black*))
 
 (defun main ()
-  (write-line "Starting game")
   (sdl:with-init ()
-    (setf *window* (sdl:window *screen-width* *screen-height*
-			       :sw t
-			       :position t
+    (setf *window* (sdl:window +screen-width+ +screen-height+
 			       :title-caption "Falling Blocks Game"
 			       :icon-caption "Falling Blocks Game"))
     (unless *window*
@@ -20,12 +40,7 @@
     (sdl:with-events ()
       (:quit-event () t)
       (:idle ()
-	     (sdl:draw-filled-circle-* (random 400) (random 400) (random 40)
-				       :color (sdl:color :r 255
-							 :g 255
-							 :b 255
-							 :a 255)
-				       :alpha 255))
+	     (render-gui))
       (:key-down-event (:key k)
 		       (cond
 			 ((sdl:key= k :sdl-key-escape)
